74cee406fa25d25df8fc9946c47417e5f58bece8
caclm10 <lewinxander@gmail.com>
2024-08-27 00:46:26 +0700

    feat: add invalid state

--- resources/js/Components/UI/Input.jsx
+++ resources/js/Components/UI/Input.jsx
@@ -3,22 +3,22 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 /**
- * @typedef {import("react").InputHTMLAttributes<HTMLInputElement>} InputProps
+ * @typedef {import("react").InputHTMLAttributes<HTMLInputElement> & {isInvalid?: boolean}} InputProps
  */
 
 const Input = React.forwardRef(
     /**
-     * @function Input
+     * 
      * @param {InputProps} props
      * @param {React.ForwardedRef<HTMLInputElement>} ref
-     * @returns {JSX.Element}
      */
-    ({ className, type = "text", ...props }, ref) => {
+    ({ className, type = "text", isInvalid = false, ...props }, ref) => {
         return (
             <input
                 type={type}
                 className={cn(
-                    "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+                    "flex h-10 w-full rounded-md border bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+                    isInvalid ? "border-destructive focus-visible:ring-destructive" : "border-input focus-visible:ring-ring",
                     className
                 )}
                 ref={ref}
@@ -31,18 +31,15 @@ Input.displayName = "Input"
 
 /**
  * @typedef {import("@tabler/icons-react").IconProps} IconProps
- */
-
-/**
+ * 
  * @typedef {{icon: React.ForwardRefExoticComponent<IconProps & React.RefAttributes<import("@tabler/icons-react").Icon>>}} InputIconProps
  */
 
 const InputIcon = React.forwardRef(
     /**
-     * @function InputIcon
+     *
      * @param {InputIconProps} props
      * @param {React.ForwardedRef<import("@tabler/icons-react").Icon>} ref
-     * @returns {React.JSX.Element}
      */
     function InputIcon(
         { icon, stroke = 1.5, className, ...props },
