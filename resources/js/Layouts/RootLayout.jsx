1ddbbb8b9fff1c2bbcd60354f0488fcbf481fcec
caclm10 <lewinxander@gmail.com>
2024-08-26 13:52:29 +0700

    feat: add root layout
    handle the page title in root layout instead on app.jsx file

---
+++ resources/js/Layouts/RootLayout.jsx
@@ -0,0 +1,28 @@
+import { Toaster } from "sonner";
+
+/**
+ * @typedef {Object} RootLayoutProps
+ * @property {string} [title]
+ * @property {React.ReactNode} [children]
+ */
+
+const appName = import.meta.env.VITE_APP_NAME || "SCards";
+
+/** @param {RootLayoutProps} props */
+function RootLayout({ title, children }) {
+    const fullTitle = title ? `${title} - ${appName}` : appName;
+
+    return (
+        <>
+            <Head title={fullTitle} />
+
+            {children}
+
+            <Toaster richColors />
+        </>
+    );
+}
+
+export { RootLayout, RootLayoutProps };
+
+
