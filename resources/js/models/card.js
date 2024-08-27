75646532aa7807e958bd147cfac03e6a99fdfbab
caclm10 <lewinxander@gmail.com>
2024-08-27 23:33:01 +0700

    feat: add card image model jsdoc

--- resources/js/models/card.js
+++ resources/js/models/card.js
@@ -1,8 +1,15 @@
 /**
+ * @typedef {Object} CardImage
+ * @property {number} id
+ * @property {string} url
+ */
+
+/**
  * @typedef {Object} Card
  * @property {number} id
  * @property {string} title
  * @property {string} content
+ * @property {CardImage[]} images
  */
 
 export { }
