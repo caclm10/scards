73146dea12dfd99648878db029a3e25b49b2857c
caclm10 <lewinxander@gmail.com>
2024-08-27 23:37:02 +0700

    chore: swiper style (swiper installed on commit 28bad98)

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,18 +1,23 @@
-import './bootstrap';
-import '../css/app.css';
+import "./bootstrap"
 
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from "react-dom/client"
+import { createInertiaApp } from "@inertiajs/react"
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers"
+
+import "swiper/css"
+import "swiper/css/free-mode"
+import "swiper/css/navigation"
+import "swiper/css/thumbs"
+import "../css/app.css"
 
 createInertiaApp({
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob("./Pages/**/*.jsx")),
     setup({ el, App, props }) {
-        const root = createRoot(el);
+        const root = createRoot(el)
 
-        root.render(<App {...props} />);
+        root.render(<App {...props} />)
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
-});
+})
