548a00278bb7f68bbf3f403208fe2523b45ac87f
caclm10 <lewinxander@gmail.com>
2024-08-28 13:00:48 +0700

    feat: add filepond package

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,13 +2,20 @@ import "./bootstrap"
 
 import { createRoot } from "react-dom/client"
 import { createInertiaApp } from "@inertiajs/react"
+import { registerPlugin } from "react-filepond"
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation"
+import FilePondPluginImagePreview from "filepond-plugin-image-preview"
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers"
 
 import "swiper/css"
 import "swiper/css/free-mode"
 import "swiper/css/navigation"
 import "swiper/css/thumbs"
+import "filepond/dist/filepond.min.css"
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css"
 import "../css/app.css"
+
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview)
 
 createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob("./Pages/**/*.jsx")),
