9bd7476a65962e7f300d6955f18bdcb478a048bb
caclm10 <lewinxander@gmail.com>
2024-08-29 10:53:19 +0700

    feat: redirect from index page

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,10 @@ use App\Http\Controllers\Account\ProfileController;
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\CardController;
 use App\Http\Controllers\CardImageController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+
+Route::redirect('/', '/cards');
 
 Route::middleware('auth')->group(function () {
     Route::prefix('account')->name('account.')->group(function () {
