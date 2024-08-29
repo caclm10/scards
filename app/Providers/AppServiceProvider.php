79faf3a4d28bfa30a330795a93a43240ca00af62
caclm10 <lewinxander@gmail.com>
2024-08-29 11:29:28 +0700

    fix: configuration for force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('APP_ENV') === 'production') {
+        if ($this->app->environment() === 'production') {
             URL::forceScheme('https');
         }
     }
