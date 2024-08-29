e6088933ad80d62255dd1b00f5286218f7bfff8a
caclm10 <lewinxander@gmail.com>
2024-08-29 11:42:51 +0700

    feat: remove padding for card list wrapper

--- resources/js/Components/Card/CardList.jsx
+++ resources/js/Components/Card/CardList.jsx
@@ -8,7 +8,7 @@ import { CardListItem } from "./CardListItem"
 /** @param {CardListProps} props */
 function CardList({ data }) {
     return (
-        <section className="grid gap-6 px-4 py-8 md:grid-cols-2 lg:px-6 xl:grid-cols-3">
+        <section className="grid md:grid-cols-2 xl:grid-cols-3">
             {data.map((item) => (
                 <CardListItem key={item.id} item={item} />
             ))}
