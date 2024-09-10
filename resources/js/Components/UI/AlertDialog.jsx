d3bb858af071bb554a617adc6829d7011e114039
caclm10 <lewinxander@gmail.com>
2024-09-10 17:24:04 +0700

    feat: change alert dialog style

--- resources/js/Components/UI/AlertDialog.jsx
+++ resources/js/Components/UI/AlertDialog.jsx
@@ -12,7 +12,7 @@ const AlertDialogPortal = AlertDialogPrimitive.Portal
 
 const AlertDialogOverlay = React.forwardRef(
     /**
-     * 
+     *
      * @param {React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Overlay>} props
      * @param {React.ElementRef<typeof AlertDialogPrimitive.Overlay>} ref
      */
@@ -30,7 +30,7 @@ AlertDialogOverlay.displayName = AlertDialogPrimitive.Overlay.displayName
 
 const AlertDialogContent = React.forwardRef(
     /**
-     * 
+     *
      * @param {React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Content>} props
      * @param {React.ElementRef<typeof AlertDialogPrimitive.Content>} ref
      */
@@ -40,7 +40,7 @@ const AlertDialogContent = React.forwardRef(
             <AlertDialogPrimitive.Content
                 ref={ref}
                 className={cn(
-                    "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
+                    "fixed left-[50%] top-[50%] z-50 grid w-[90%] max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-lg",
                     className
                 )}
                 {...props}
@@ -53,7 +53,7 @@ AlertDialogContent.displayName = AlertDialogPrimitive.Content.displayName
 const AlertDialogHeader = ({ className, ...props }) => (
     <div
         className={cn(
-            "flex flex-col space-y-2 text-center sm:text-left",
+            "flex flex-col space-y-2 text-justify",
             className
         )}
         {...props}
@@ -65,7 +65,7 @@ AlertDialogHeader.displayName = "AlertDialogHeader"
 const AlertDialogFooter = ({ className, ...props }) => (
     <div
         className={cn(
-            "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
+            "flex flex-row justify-end space-x-2",
             className
         )}
         {...props}
@@ -75,7 +75,7 @@ AlertDialogFooter.displayName = "AlertDialogFooter"
 
 const AlertDialogTitle = React.forwardRef(
     /**
-     * 
+     *
      * @param {React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Title>} props
      * @param {React.ElementRef<typeof AlertDialogPrimitive.Title>} ref
      */
@@ -90,7 +90,7 @@ AlertDialogTitle.displayName = AlertDialogPrimitive.Title.displayName
 
 const AlertDialogDescription = React.forwardRef(
     /**
-     * 
+     *
      * @param {React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Description>} props
      * @param {React.ElementRef<typeof AlertDialogPrimitive.Description>} ref
      */
@@ -105,7 +105,7 @@ AlertDialogDescription.displayName = AlertDialogPrimitive.Description.displayNam
 
 const AlertDialogAction = React.forwardRef(
     /**
-     * 
+     *
      * @param {React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Action> & {variant?: import("./Button").ButtonVariants["variant"]}} props
      * @param {React.ElementRef<typeof AlertDialogPrimitive.Action>} ref
      */
@@ -120,7 +120,7 @@ AlertDialogAction.displayName = AlertDialogPrimitive.Action.displayName
 
 const AlertDialogCancel = React.forwardRef(
     /**
-     * 
+     *
      * @param {React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Cancel>} props
      * @param {React.ElementRef<typeof AlertDialogPrimitive.Cancel>} ref
      */
@@ -129,7 +129,6 @@ const AlertDialogCancel = React.forwardRef(
             ref={ref}
             className={cn(
                 buttonVariants({ variant: "outline" }),
-                "mt-2 sm:mt-0",
                 className
             )}
             {...props}
