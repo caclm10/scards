0b2e8284b8138f0a4251249fc7f47f320afed825
caclm10 <lewinxander@gmail.com>
2024-09-10 15:57:35 +0700

    fix: move http schema config to boot method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,9 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment() === 'production') {
-            URL::forceScheme('https');
-        }
+        //
     }
 
     /**
@@ -22,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        
+        if ($this->app->environment() === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
